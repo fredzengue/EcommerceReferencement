750d808183a30deb39efb3cb57a134a043b90056
fred zengue <fredzengue@gmail.com>
2022-04-06 17:08:07 +0200

    make products model

---
+++ database/migrations/2022_04_06_143357_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image');
+            $table->float('price');
+            $table->text('description');
+            $table->unsignedBigInteger('id_subcategory');
+            $table->foreign('id_subcategory')
+                  ->references('id')
+                  ->on('subcategories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
