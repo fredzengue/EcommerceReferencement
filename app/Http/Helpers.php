d5527b1bf26add09b7bb7c9cc454ae14723f437d
fred zengue <fredzengue@gmail.com>
2022-04-07 12:37:01 +0200

    comment subcategories list on layouts.master

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -2,12 +2,13 @@
 
 use App\Models\Category;
 use App\Models\Country;
+use App\Models\Subcategory;
 
 function getCategories() {
     return Category::all();
 }
 function getCategoriesSubcategories(){
-    return Category::all();
+    return Subcategory::all();
 }
 function getCountriesCategories() {
     return Country::with('categories')->get();
