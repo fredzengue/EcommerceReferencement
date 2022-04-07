570b80d333aa9bdd85336c861caef6d35b33c1e8
fred zengue <fredzengue@gmail.com>
2022-04-07 10:08:09 +0200

    added postgres database configuration

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="Pkz_40iX2mhipnf0fKOAaNDFY0JivhzL91-3Tx5pGBo" />
     <link rel="profile" href="//gmpg.org/xfn/11">
     <title>Organico || @yield('page_title')</title>
     <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
