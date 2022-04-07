c93675b432750ac1e06ba49fad13eca8a1914eee
fred zengue <fredzengue@gmail.com>
2022-04-07 12:17:35 +0200

    add seed

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
 use App\Models\Product;
-use App\Models\Subcategory;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
