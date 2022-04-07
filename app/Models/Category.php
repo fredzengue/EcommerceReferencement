a991195605787c53f50f0b5882f580558936cefd
fred zengue <fredzengue@gmail.com>
2022-04-07 12:43:43 +0200

    comment subcategories list on layouts.master

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -13,7 +13,7 @@ class Category extends Model
     }
     public function subcategories()
     {
-        return $this->hasMany(App\Models\ProductController::class);
+        return $this->hasMany(App\Models\Subcategory::class);
     }
 
 }
